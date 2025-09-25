01e3555742cf92b54c4ca5bbf36f212e0f91dfeb
Moussa <moussachaabar@hotmail.com>
2025-09-25 10:54:46 +0200

    Remove dashboard/profile scaffolding

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,11 @@ use App\Http\Controllers\LeaderboardController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware(['auth'])->group(function () {
-    //Route::get('/games/create', [GameController::class, 'create'])->name('games.create');
     Route::get('/leaderboard', [LeaderboardController::class, 'index'])->name('leaderboard');
     Route::post('/games', [GameController::class, 'store'])->name('games.store');
 });
 
 Route::get('/', function () { return redirect()->route('leaderboard'); })->name('home');
 
-require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
